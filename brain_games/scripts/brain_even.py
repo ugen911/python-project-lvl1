640b2c4c63234d3912a90dfcd87e71cf25271d1c
EvgeniStroikin <evgenistroikin@gmail.com>
2022-08-27 17:03:52 +0700

    brain_even module

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python3
 
-from brain_games.scripts.brain_games import main
 from random import randint
 import prompt
 
-
+def welcome_user():
+    username = prompt.string('May I have your name? ')
+    print(f'Hello {username}!')
+    return username
 
 def even_number():
+    username = welcome_user()
     counter_right_answer = 0
-    print('Answer "yes" if the number is even, otherwise answer "no".')
     
     def is_right_answer():
+        right_answer = ''
+        print('Answer "yes" if the number is even, otherwise answer "no".')
         random_number = randint(0,100)
         print(f'Question:{random_number}')
-        answer = prompt.string('Your answer: ')  
-        if random_number // 2 == 0 and answer == 'yes' or random_number // 2 == 1 and answer == 'no':
+        answer = prompt.string('Your answer: ')
+        if random_number % 2 == 0:
+            right_answer = 'yes'
+        else:
+            right_answer = 'no'
+        if answer == right_answer:
+            print('Correct')
             return True
-        return answer
+        else:
+            print(f'''"{answer}" is wrong answer ;(. Correct answer was "{right_answer}".
+            Let's try again, {username}''')
+
     
     while counter_right_answer < 3:
-        if is_right_answer() == True:
-            print('Correct')
+        if is_right_answer():
             counter_right_answer += 1
         else:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'./nLet's try again, Bill!")
             break
-    print("Congratulations, Bill!")
+    if counter_right_answer == 3:
+        print (f'Congratulation {username}')    
+        
+    
 
+
+
+def main():
     even_number()
-
-
-
-
 
 
 if __name__ == '__main__':
