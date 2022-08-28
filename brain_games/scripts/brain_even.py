3353ca66fe96e38f5c50448a3c28e1d86d1ee54d
EvgeniStroikin <evgenistroikin@gmail.com>
2022-08-28 11:25:26 +0700

    after make lint brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,19 +3,21 @@
 from random import randint
 import prompt
 
+
 def welcome_user():
     username = prompt.string('May I have your name? ')
     print(f'Hello {username}!')
     return username
 
+
 def even_number():
     username = welcome_user()
     counter_right_answer = 0
-    
+
     def is_right_answer():
         right_answer = ''
         print('Answer "yes" if the number is even, otherwise answer "no".')
-        random_number = randint(0,100)
+        random_number = randint(0, 100)
         print(f'Question:{random_number}')
         answer = prompt.string('Your answer: ')
         if random_number % 2 == 0:
@@ -29,15 +31,13 @@ def even_number():
             print(f'''"{answer}" is wrong answer ;(. Correct answer was "{right_answer}".
             Let's try again, {username}''')
 
-    
     while counter_right_answer < 3:
         if is_right_answer():
             counter_right_answer += 1
         else:
             break
     if counter_right_answer == 3:
-        print (f'Congratulation {username}')    
-        
+        print(f'Congratulation {username}')
 
 
 def main():
