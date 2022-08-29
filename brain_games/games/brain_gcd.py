b8a2cfcc7b7aac72f9f2c4fb2e9576f3aff106a5
“Stroikin” <evgenistroikin@gmail.com>
2022-08-29 13:44:10 +0700

    brain-gcd ready, version 0.3.1

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -2,10 +2,29 @@
 from brain_games.cli import welcome_user
 from brain_games.scripts.game_logic import checking_correct
 from random import randint
+import math
 
 
-def gcd():
-    return True
+task = 'Find the greatest common divisor of given numbers.'
+
+def gcd(task=task):
+    round_count = 3
+    username = welcome_user()
+    counter_right_answer = 0
+    while counter_right_answer < round_count:
+        random_number1 = randint(0, 100)
+        random_number2 = randint(0, 100)
+        question = str(random_number1)+" "+str(random_number2)
+        right_answer = str(math.gcd(random_number1, random_number2))
+        result = checking_correct(task=task, question=question, 
+                                  right_answer=right_answer, 
+                                  user_name=username)
+        if result:
+            counter_right_answer +=1 
+        else:
+            break
+    if counter_right_answer == round_count:
+        print(f'Congratulation {username}')
 
 def main():
     gcd()
