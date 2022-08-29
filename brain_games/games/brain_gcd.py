ac5660c9c77b0ee7a86a5ca718fd53a472ff60b9
“Stroikin” <evgenistroikin@gmail.com>
2022-08-29 13:15:35 +0700

    brain_gcd init full

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+from brain_games.scripts.game_logic import checking_correct
+from random import randint
+
+
+def gcd():
+    return True
+
+def main():
+    gcd()
+
+
+if __name__ == '__main__':
+    main()
