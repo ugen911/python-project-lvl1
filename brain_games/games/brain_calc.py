211dfc1785a3fa39600b9ad29a6ea289076bc106
“Stroikin” <evgenistroikin@gmail.com>
2022-08-29 11:26:38 +0700

    brain_even is ready, game_logic.py includes total logit for games

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+from brain_games.scripts.game_logic import checking_correct
+from random import randint
+
+def calc():
+    return True
+
+
+
+def main():
+    calc()
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/game_logic.py
+++ brain_games/scripts/game_logic.py
@@ -1,4 +1,10 @@
-def checking_correct(user_answer, right_answer, user_name):
+import prompt
+
+
+def checking_correct(task, question, right_answer, user_name):
+    print(task)
+    print(question)
+    user_answer = prompt.string('Your answer: ')
     if user_answer == right_answer:
             print('Correct')
             return True
@@ -6,12 +12,25 @@ def checking_correct(user_answer, right_answer, user_name):
         print(f'''"{user_answer}" is wrong answer ;(. Correct answer was "{right_answer}".
         Let's try again, {user_name}''')
     
-def counter_game_round(user_name):
-    counter_right_answer = 0
-    while counter_right_answer < 3:
-        if is_right_answer():
-            counter_right_answer += 1
-        else:
-            break
-    if counter_right_answer == 3:
-        print(f'Congratulation {user_name}')
+
+
+
+
+
+
+# def is_right_answer():
+#         right_answer = ''
+#         print('Answer "yes" if the number is even, otherwise answer "no".')
+#         random_number = randint(0, 100)
+#         print(f'Question:{random_number}')
+#         answer = prompt.string('Your answer: ')
+#         if random_number % 2 == 0:
+#             right_answer = 'yes'
+#         else:
+#             right_answer = 'no'
+#         if answer == right_answer:
+#             print('Correct')
+#             return True
+#         else:
+#             print(f'''"{answer}" is wrong answer ;(. Correct answer was "{right_answer}".
+#             Let's try again, {username}''')
