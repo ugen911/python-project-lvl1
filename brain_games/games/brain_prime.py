c7b6c520565539f196903386fb0de9d7ae42ac2a
“Stroikin” <evgenistroikin@gmail.com>
2022-08-29 17:37:15 +0700

    processing is_prime func

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -11,9 +11,12 @@ def is_prime_number():
     counter_right_answer = 0
     
     while counter_right_answer < round_count:
-        right_answer = 'no'
+        right_answer = 'yes'
         random_number = randint(0, 100)
         question = str(random_number)
+        for i in range(2, random_number):
+            if random_number % i == 0:
+                right_answer = 'yes'
        
         result = checking_correct(task=task, question=question, 
                                   right_answer=right_answer, 
